e0611b7b9d7bb9570da667f241c2976378139fe1
Diego Blanco <diego_blanco@msn.com>
2026-05-06 15:19:25 -0300

    Aula_06maio
    Arquivos finais

--- CKT2.vhd
+++ CKT2.vhd
@@ -1,13 +1,16 @@
--- Exemplo: Declaração de uma porta NAND em VHDL
 
 library std;
 use std.standard.all;
 
 entity CKT2 is
-	port (a, b, c, d: in bit; y: out bit);
+	port (a, b, c, d: in bit;
+	y: out bit
+  );
 end entity;
 
 architecture ifsc_v1 of CKT2 is
 begin
-	x <= a nand b;
+
+y <= not (a and b) or (c and (not d) and (not b));
+
 end architecture;
